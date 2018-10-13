41ed0c1dc06c53576d8d5069a056ea2902e2b24a
gui <gui@MacBook-Pro-2.local>
2018-10-14 00:43:33 +0800

    修改时间

--- models/sub/subresult.go
+++ models/sub/subresult.go
@@ -2,13 +2,13 @@ package sub
 
 import (
 	"github.com/MobileCPX/PreMondia/models"
-	"github.com/MobileCPX/nth/util"
+	"github.com/MobileCPX/PreMondia/util"
 	"github.com/astaxie/beego/orm"
 )
 
 // InsertSubscribe 插入订阅通知
 func InsertSubscribe(sub *models.MdSubscribe) {
 	o := orm.NewOrm()
-	sub.Sendtime, _ = util.GetFormatTime()
+	sub.Sendtime, _ = util.GetDatetime()
 	o.Insert(sub)
 }
